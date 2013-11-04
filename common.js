e806fec4c26bde9a64963af5bb6bbf2a96b7f794
Alex Miller <alex.k.miller@gmail.com>
2013-11-04 16:55:17 +0100

    Styled homepage and appeal overview

--- common.js
+++ common.js
@@ -17,8 +17,19 @@ for(var i = 0; i < VariableArray.length; i++){
 var KeyValuePair = VariableArray[i].split('=');
 if(KeyValuePair[0] == VarSearch){
 return KeyValuePair[1];}}}
+function truncnumbermagnitude(x) {
+if(x>=10000000000){magnitude="BILLION";return (x/1000000000).toFixed(1)}
+if(x<10000000000&x>=1000000000){magnitude="BILLION";return (x/1000000000).toFixed(2)}
+if(x<1000000000&x>=100000000){magnitude="MILLION"; return (x/1000000).toFixed(0)}
+if(x<100000000&x>=10000000){magnitude="MILLION"; return (x/1000000).toFixed(1)}
+if(x<10000000&x>=1000000){magnitude="MILLION"; return (x/1000000).toFixed(2)}
+if(x<1000000&x>=100000){magnitude="THOUSAND"; return (x/1000).toFixed(0)}
+if(x<100000&x>=10000){magnitude="THOUSAND"; return (x/1000).toFixed(1)}
+if(x<1000000&x>=1000){magnitude="THOUSAND"; return (x/1000).toFixed(2)}
+if(x<1000){magnitude=""; return (x)};}
 function truncnumber(x) {
-if(x>=1000000000){return (x/1000000000).toFixed(2)+"B"}
+if(x>=10000000000){return (x/1000000000).toFixed(1)+"B"}
+if(x<10000000000&x>=1000000000){return (x/1000000000).toFixed(2)+"B"}
 if(x<1000000000&x>=100000000){return (x/1000000).toFixed(0)+"M"}
 if(x<100000000&x>=10000000){return (x/1000000).toFixed(1)+"M"}
 if(x<10000000&x>=1000000){return (x/1000000).toFixed(2)+"M"}
@@ -62,4 +73,29 @@ $('nav#menu').mmenu({
         counters: true
 });
 });
-$(document).ready(function(){$('li.top em').hide();});
+$(document).ready(function(){$('li.top em').text("BY YEAR");
+                  $('li.types em').hide();});
+donutindex = 0
+donut = function(radius,covered,text1,text2,parent){
+$(document).ready(function(){
+    if(covered>=1){covered=.999999}
+    if(covered<=0){covered=.000001}
+    var percentremain = 1-covered,
+    a = Math.PI*2*percentremain,
+    r = radius,
+    x= -r + (r*(Math.cos(a))),
+    y= (r*(Math.sin(a)));
+
+$(parent).append("<svg width='"+radius*2+"px' height='"+radius*2+"px'><g transform='translate("+radius*2+","+radius*2+") scale(1,-1) rotate(90)'><path fill='#0094cc' class='remain' id='donut"+donutindex+"'/><path fill='#0f6cb6' class='commit' id='donut"+donutindex+"'/><circle fill='#11c0f3' r='"+radius*(3/5)+"' cx='"+radius+"' cy='"+radius+"'/></g><text id='text1' fill='#FFF' x='"+parseFloat(radius-27)+"' y='"+parseFloat(radius+4)+"' font-size='25'>"+text1+"</text><text id='text2' fill='#FFF' x='"+parseFloat(radius-25)+"' y='"+parseFloat(radius+15)+"'>"+text2+"</text></svg>")
+
+if (percentremain>0.5) {$("path#donut"+donutindex+".remain").attr("d","M"+r+","+r+" h"+r+" a"+r+","+r+" 0 1,1 "+x+","+y+" z")}
+else {$("path#donut"+donutindex+".remain").attr("d","M"+r+","+r+" h"+r+" a"+r+","+r+" 0 0,1 "+x+","+y+" z")}
+if (1-percentremain<0.5) {$("path#donut"+donutindex+".commit").attr("d","M"+r+","+r+" h"+r+" a"+r+","+r+" 0 0,0 "+x+","+y+" z")}
+else {$("path#donut"+donutindex+".commit").attr("d","M"+r+","+r+" h"+r+" a"+r+","+r+" 0 1,0 "+x+","+y+" z")}
+donutindex +=1
+});}
+progressbar = function(covered,width,height){
+var coveredwidth = Math.round(width*covered),
+progressbar = "<svg id='progressbar' viewBox='0 0 "+width+" "+height+"'><rect id='total' width='"+width+"' height='"+height+"' fill='#ffe9b5'/><rect id='covered' width='"+coveredwidth+"' height='"+height+"' fill='#fcaf17'/></svg>";
+return progressbar;
+}
